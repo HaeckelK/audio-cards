6b45fe7631f53e1ab8f4644403c447363cab8b10
HaeckelK <haeckelk.github@gmail.com>
2021-06-03 07:42:00 +0000

    feat: language table

--- app/app.py
+++ app/app.py
@@ -18,14 +18,34 @@ class Word(db.Model):
     def __repr__(self):
         return '<Word %r>' % self.word
 
-db.create_all()
+
+class Language(db.Model):
+    id = db.Column(db.String(2), primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<Language %r>' % self.id
+
+
+db.create_all() 
 db.session.commit()
+
+# TODO move crud to own module
+def add_language(code: str, name: str, session) -> None:
+    language = Language(id=code.lower(), name=name.lower())
+    session.add(language)
+    session.commit()
+    return
+
+add_language(code="DE", name="German", session=db.session)
+add_language(code="FR", name="French", session=db.session)
 
 
 @app.route('/')
 def index():
     words = Word.query.all()
-    return render_template("index.html", words=words)
+    languages = Language.query.all()
+    return render_template("index.html", words=words, languages=languages)
 
 
 @app.route("/add_word", methods=["POST"])
